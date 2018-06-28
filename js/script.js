f14f708c27505fab13ebc337feb3c68c3a83441b
Stephan Griesel <sgriesel@gmail.com>
2018-06-28 21:03:27 +0200

    add event listener & functions

--- js/script.js
+++ js/script.js
@@ -22,3 +22,35 @@ const inputField = document.querySelector('#input-field');
 const inputLabel = document.querySelector('#input-label');
 const inputProgress = document.querySelector('#input-progress');
 const progress = document.querySelector('#progress-bar');
+
+//Events
+document.addEventListener('DOMContentLoaded', getQuestion);
+
+// Functions 
+
+// Get question from array & add to page
+function getQuestion() {
+    // Get Current Questions
+    inputLabel.innerHTML = questions[position].question;
+    // Get Current Type 
+    inputField.type = questions[position].type || 'text';
+    // Get Current Answer
+    inputField.value = questions[position].answer || '';
+    // Focus On Element
+    inputField.focus();
+
+    // Set Progress Bar Width - Variable to the questions length
+    progress.style.width = (position * 100) / questions.length + '%';
+
+    // Add user icon or nav depending on question 
+    prevBtn.className = position ? 'fas fa-arrow-left' : 'fas fa-user';
+
+    showQuestion();
+}
+
+// Display question
+function showQuestion() {
+    inputGroup.style.opacity = 1;
+    inputProgress.style.transition = '';
+    inputProgress.style.width = '100%';
+}
